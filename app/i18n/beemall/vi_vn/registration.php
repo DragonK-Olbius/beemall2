8461dfc950e56574eb234fc4915e22f1c5e67eb8
DragonK-Olbius <lethiep.bk@gmail.com>
2018-07-20 23:06:15 +0900

    first commit

---
+++ app/i18n/beemall/vi_vn/registration.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'beemall_vi_vn',
+    __DIR__
+);
